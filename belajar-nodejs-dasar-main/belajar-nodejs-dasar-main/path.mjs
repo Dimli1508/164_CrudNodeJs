edef44a5a75508bccfbbfb25e5647028d27d8373
Mashell0707 <merlsutama@gmail.com>
2024-11-02 20:54:35 +0700

    164_CrudNodeJs

---
+++ belajar-nodejs-dasar-main/belajar-nodejs-dasar-main/path.mjs
@@ -0,0 +1,9 @@
+import path from "path";
+
+const file = "/Users/asroni/contoh.txt";
+
+//const file = "/Users/asroni/Library/CloudStorage/OneDrive-UniversitasMuhammadiyahYogyakarta/Kuliah TI/Genap 2023-2024/PDW/Materi/";
+
+console.info(path.dirname(file));
+console.info(path.basename(file));
+console.info(path.extname(file));
